13d0e387e4a3ba143fa2a46c979182349e4e624c
Andreas Pätzold <andi.paetzold@gmail.com>
2015-07-28 21:47:36 +0200

    Added IntervalValuePair

---
+++ IntervalDictionary/IntervalValuePair.cs
@@ -0,0 +1,23 @@
+﻿namespace IntervalDictionary
+{
+    using System;
+
+    public struct IntervalValuePair<TBound, TValue>
+        where TBound : IComparable<TBound>, IEquatable<TBound>
+    {
+        public IntervalValuePair(IInterval<TBound> interval, TValue value)
+            : this()
+        {
+            Interval = interval;
+            Value = value;
+        }
+
+        public IInterval<TBound> Interval { get; private set; }
+        public TValue Value { get; private set; }
+
+        public override string ToString()
+        {
+            return string.Format("{0}: {1}", Interval, Value);
+        }
+    }
+}
