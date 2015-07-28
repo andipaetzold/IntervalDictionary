90e75d04c198df4a95b8adb9b1cbc5bf724d4223
Andreas Pätzold <andi.paetzold@gmail.com>
2015-07-28 20:38:52 +0200

    Added Interval Interface

---
+++ IntervalDictionary/IInterval.cs
@@ -0,0 +1,25 @@
+﻿namespace IntervalDictionary
+{
+    using System;
+
+    public interface IInterval<TBound> : IComparable<TBound>
+        where TBound : IComparable<TBound>, IEquatable<TBound>
+    {
+        TBound LowerBound { get; set; }
+        TBound UpperBound { get; set; }
+
+        bool Contains(TBound bound);
+
+        bool Intersects(IInterval<TBound> other);
+
+        bool IsSubsetOf(IInterval<TBound> other);
+
+        bool IsSupersetOf(IInterval<TBound> other);
+
+        IInterval<TBound> Intersect(IInterval<TBound> other);
+
+        IInterval<TBound> Union(IInterval<TBound> other);
+
+        IInterval<TBound> Subtract(IInterval<TBound> other);
+    }
+}
