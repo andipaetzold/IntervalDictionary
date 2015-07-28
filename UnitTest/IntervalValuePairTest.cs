ae360a06abf15792d76e83537c366bf57d4db553
Andreas Pätzold <andi.paetzold@gmail.com>
2015-07-29 01:45:42 +0200

    Added tests for IntervalValuePair

---
+++ UnitTest/IntervalValuePairTest.cs
@@ -0,0 +1,21 @@
+﻿namespace UnitTest
+{
+    using IntervalDictionary;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    [TestClass]
+    public class IntervalValuePairTest
+    {
+        #region ToString
+
+        [TestMethod]
+        public void String()
+        {
+            var intervalValuePair = new IntervalValuePair<int, string>(new Interval<int>(5, 10), "5 - 10");
+
+            Assert.AreEqual("[5, 10]: 5 - 10", intervalValuePair.ToString());
+        }
+
+        #endregion
+    }
+}
